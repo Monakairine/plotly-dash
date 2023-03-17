9bdd6c76fc5ce337c52a26be682e45a6edd33b6b
Monalisa <monalisa.pereira@taqtile.com.br>
2023-03-17 16:43:10 -0300

    example 8 and 9

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 from datetime import datetime, timedelta, date
 import random
+import plotly.graph_objs as go
 
 app = Dash(__name__)
 server = app.server
@@ -28,6 +29,7 @@ df_sales = pd.DataFrame({
 #lista de figuras
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 fig2 = px.line(df_sales, x="Date", y="Total Sold")
+fig3 = px.bar(df_sales, x='Date', y='Total Sold')
 
 
 app.layout = html.Div(children=[
@@ -80,55 +82,136 @@ app.layout = html.Div(children=[
         ]),
     ], style={'width': '50%', 'display': 'inline-block'}),
 
-    html.H3(children='''
+    html.Div(children=[
+        html.H3(children='''
         Example 4: line chart with a date picker range
     '''),
 
-    # Add a date picker
-    dcc.DatePickerRange(
-        id='my-date-picker-range',
-        start_date=date.today() - timedelta(days=14),
-        end_date=date.today(),
-        max_date_allowed=date.today()
-    ),
+        # Add a date picker
+        dcc.DatePickerRange(
+            id='my-date-picker-range',
+            start_date=date.today() - timedelta(days=14),
+            end_date=date.today(),
+            max_date_allowed=date.today()
+        ),
 
-    dcc.Graph(
-        id='example-graph-4',
-        figure=fig2
-    ),
+        dcc.Graph(
+            id='example-graph-4',
+            figure=fig2
+        ),
+    ]
+        , style={'width': '60%', 'display': 'inline-block'}),
 
+   
+   html.Div(children=[
+       html.H3(children='''
+        Example 6: bar chart with date picker range
+    '''),
 
+       dcc.Graph(id='example-graph-6'),
+   ], style={'width': '40%', 'display': 'inline-block'}),
+
+   
     html.H3(children='''
-        Example 5: text input
+        Example 7: text input
     '''),
 
     html.Div(children='''
         Enter text to add a new div:
     '''),
 
-    dcc.Input(id='example5-input', value='', type='text'),
+    html.Div(children=[
+        dcc.Input(id='example7-input', value='', type='text'),
+        html.Button('Add', id='example7-button'),
+    ]),
+    html.Div(id='example7-output'),
 
-    html.Div(id='example5-output'),
+    html.H3(children='''
+        Example 8: text area
+    '''),
+
+    html.Textarea(),
+
+    html.H3(children='''
+        Example 9: Gráfico de barras dinâmico
+    '''),
+
+    html.P(children='''
+        Insira dados separados por vírgula ou quebra de linha. Após isto, surgirá um gráfico que conta quantas vezes um dado elemento aparece na lista
+    '''),
+
+    html.Div([
+        dcc.Textarea(
+            id='input-box-example9',
+            placeholder='Insira dados separados por vírgula ou quebra de linha',
+            style={'width': '100%', 'height': 100}
+        ),
+        html.Button('Gerar Gráfico', id='button9'),
+    ]),
+
+    dcc.Graph(id='output-graph9'),
 
 ])
 
 
-@app.callback(
-    Output('example5-output', 'children'),
-    Input('example5-input', 'value'),
-    State('example5-output', 'children')
-)
-def add_div(value, existing_children):
 
-    existing_children = html.Div()
-    if value:
+
+#------------- Exemplo 9 -------------------------
+@app.callback(Output('output-graph9', 'figure'),
+              Input('button9', 'n_clicks'),
+              State('input-box-example9', 'value'))
+def update_figure(n_clicks, input_value):
+    if input_value:
+        data = input_value.strip().replace('\n', ',').replace(',', ' ').split()
+        df = pd.DataFrame({'Data': data})
+        counts = df['Data'].value_counts()
+        fig = go.Figure(data=[go.Bar(x=counts.index, y=counts)])
+        fig.update_layout(title='Contagem de Dados',
+                          xaxis_title='Dados', yaxis_title='Contagem')
+        return fig
+    else:
+        return {}
+
+#--------------- Exemplo 7 -------------------------
+@app.callback(
+    Output('example7-output', 'children'),
+    Input('example7-button', 'n_clicks'),
+    # obtem o valor atual do componente dcc.Input
+    State('example7-input', 'value'),
+    # obtem a lista atual de divs que já foram adicionados.
+    State('example7-output', 'children')
+)
+def add_div(n_clicks, value, existing_children):
+    if existing_children is None:
+        existing_children = []
+    if n_clicks and value:
+        print('entrou aqui')
+        print(existing_children)
         new_child = html.Div(children=value)
-        children = [new_child] + existing_children
+        children = [new_child]+existing_children
         return children
     else:
         return existing_children
 
 
+#--------------- Exemplo 6 -------------------------
+@app.callback(
+    Output('example-graph-6', 'figure'),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date')
+)
+def update_bar_chart(start_date, end_date):
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    # Filter the df_sales dataframe to include only the rows with dates between start_date and end_date
+    filtered_df = df_sales[df_sales['Date'].between(start_date, end_date)]
+
+    # Create a bar chart of the filtered data
+    fig = px.bar(filtered_df, x='Date', y='Total Sold')
+    return fig
+
+#--------------- Exemplo 4 -------------------------
 @app.callback(
     Output('example-graph-4', 'figure'),
     Input('my-date-picker-range', 'start_date'),
